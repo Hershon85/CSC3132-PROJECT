fa7107a16b9d1392268de72d54c3bbd6ee706fdf
David HERSHON <139619511+Hershon85@users.noreply.github.com>
2024-12-09 17:41:15 +0000

    signup php created

---
+++ php/signup.php
@@ -0,0 +1,30 @@
+<?php
+	require_once 'dbconf.php';
+	function AddData($connect,$fname,$lname,$phone,$email,$password){
+		try {
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+			$sql = "INSERT INTO user VALUES('$fname','$lname','$phone','$email','$hashed_password')";
+			$result = mysqli_query($connect,$sql);
+			if ($result) {
+				//echo "New student record created sucessfully";
+			} else {
+				die("Error ".mysqli_error($connect));
+			}
+            header('Location: ../index.html');
+            exit;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+	
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+		$fname=$_POST['fname'];
+        $lname=$_POST['lname'];
+        $phone=$_POST['phone'];
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+
+		AddData($connect,$fname,$lname,$phone,$email,$password);
+	}
+
+	?>
